93c3e5556e2a439bcf950e7ff8e1993a765f33d8
Sameer Tiwari <sameertiwari@Sameers-MacBook-Air.local>
2024-07-30 23:57:48 +0545

    feat :display recipeCard Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [recipes, setRecipes] = useState<Meal[]>([]);
-  
 
   const handleSearch = async () => {
     if (!searchTerm) return;
@@ -26,7 +25,7 @@ export default function Home() {
   };
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-20">
       <h1 className="text-4xl font-bold text-center mb-8">Recipe Search</h1>
       <div className="flex justify-center mb-8">
         <input
@@ -49,7 +48,7 @@ export default function Home() {
           Search
         </Button>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {recipes.map((recipe) => (
           <RecipeCard key={recipe.idMeal} recipe={recipe} />
         ))}
--- src/components/ReceipeCard.tsx
+++ src/components/ReceipeCard.tsx
@@ -1,24 +1,50 @@
 // components/RecipeCard.tsx
+import { Meal } from "@/app/types";
 import Link from "next/link";
-import { Meal } from "../../app/types";
+import { useState } from "react";
+import Image from "next/image";
+import { Button } from "./ui/button";
+
 interface RecipeCardProps {
   recipe: Meal;
 }
 
 export default function RecipeCard({ recipe }: RecipeCardProps) {
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-      <img
-        className="w-full h-48 object-cover"
-        src={recipe.strMealThumb}
-        alt={recipe.strMeal}
-      />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{recipe.strMeal}</div>
+    // <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+    //   <img
+    //     className="w-full h-48 object-cover"
+    //     src={recipe.strMealThumb}
+    //     alt={recipe.strMeal}
+    //   />
+    //   <div className="px-6 py-4">
+    //     <div className="font-bold text-xl mb-2">{recipe.strMeal}</div>
+    //     <Link href={`/recipe/${recipe.idMeal}`}>
+    //       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    //         View Recipe
+    //       </button>
+    //     </Link>
+    //   </div>
+    // </div>
+    <div className="rounded-sm lg:w-fill w-auto shadow-lg overflow-hidden m-4 h-[600] bg-white">
+      <div className="relative flex flex-col gap-4 justify-between">
         <Link href={`/recipe/${recipe.idMeal}`}>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            View Recipe
-          </button>
+          <div className="relative p-10 h-80 bg-slate-200 hover:opacity-55 transition hover:border-b-2 border-blue-500">
+            <Image
+              src={recipe.strMealThumb}
+              alt={recipe.strMeal}
+              objectFit="center"
+              fill
+              quality={50}
+            />
+          </div>
+
+          <div className="flex h-20 justify-start items-center">
+            <span className="text-2xl text-blue-950 hover:text-3xl transition p-4 word-wrap">
+              {recipe.strMeal}
+            </span>
+          </div>
         </Link>
       </div>
     </div>
